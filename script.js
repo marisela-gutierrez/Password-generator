0110aeacbb6662932d2982b9a57c26c5afac8bd3
marigut <mari.gutierrez96@yahoo.com>
2021-04-08 22:40:45 -0500

    password and check criteria loop

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
-// Assignment code here
+var numeric = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var specialChar = "!@#$%^&*_?{[]}~><+;:.?";
+
+
 
 // function to generate password
 var generatePassword = function() {
+
   // Enter password length
   passwordLength();
 
-  //Choose lowercase
-  confirmLowerCase();
+  //Choose password criteria
+  passwordCriteria();
 
-  //Choose uppercase
-  confirmUpperCase();
-
-  //Choose numeric
-  confirmNumeric();
-
-  //choose special characters
-  confirmSpecialChar();
-
+  // check if at least one criteria is selcted
+  checkCriteria();
+  
 };
-
 
 //function to choose password length
 var passwordLength = function() {
   var length = "";
 
-  while (length === "" || length === null) {
+  while (length === "" || length === null || length < 8 || length > 128) {
     length = prompt("Please enter a password length between 8 and 128 characters.");
   }
   console.log("Your password character length is " + length);
   return length;
 };
 
-// Function for Lower Case alpha
-var confirmLowerCase = function() {
+// Function for password criteria
+var passwordCriteria = function() {
+  var userSlection = ""; 
+  
+  lowerCase =
   window.confirm("Would you like lowercase characters in your password?");
-  // if yes (true)
-  if (confirmLowerCase) {
-   
-    return true;
+
+  //Choose uppercase
+  upperCase =
+  window.confirm("Would you like uppercase characters in your password?");
+  
+
+  //Choose numeric
+  numeric =
+  window.confirm("Would you like numeric characters in your password?");
+
+  //choose special characters
+  specialChar =
+  window.confirm("Would you like special characters in your password?");
+};
+
+var checkCriteria = function() {
+  if (
+    lowerCase === true ||
+    upperCase === true ||
+    numeric === true ||
+    specialChar === true 
+  ){
+    randomCriteria();
   }
-}; 
-
-// function for uppercase
-var confirmUpperCase = function() {
-  window.confirm("Would you like uppercase characters in your password?")
-}
-
-// function for numeric
-var confirmNumeric = function() {
-  window.confirm("Would you like numeric characters in your password?")
-}
-
-// function for special characters
-var confirmSpecialChar = function() {
-  window.confirm("Would you like special characters in your password?")
-}
+  else {
+    window.alert("At least one character type should be selected. Please try again.");
+    passwordCriteria();
+  }
+};
 
 
+
+  
+ 
 
 
 /* Generate Password*/
