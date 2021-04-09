e24829d777384529147a5dfb7d548515075a3cb5
marigut <mari.gutierrez96@yahoo.com>
2021-04-09 01:39:11 -0500

    random password

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var numeric = "0123456789";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialChar = "!@#$%^&*_?{[]}~><+;:.?";
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["!","@",":","#","$","%","^","&","*","_","?","?"];
+var password = [];
+var useChar = [];
 
 
 
@@ -16,7 +18,7 @@ var generatePassword = function() {
 
   // check if at least one criteria is selcted
   checkCriteria();
-  
+
 };
 
 //function to choose password length
@@ -32,33 +34,44 @@ var passwordLength = function() {
 
 // Function for password criteria
 var passwordCriteria = function() {
-  var userSlection = ""; 
-  
-  lowerCase =
+   
+  lowerCaseChar =
   window.confirm("Would you like lowercase characters in your password?");
+  if (lowerCaseChar) {
+    useChar = useChar.concat(lowerCase);
+  }
 
   //Choose uppercase
-  upperCase =
+  upperCaseChar =
   window.confirm("Would you like uppercase characters in your password?");
+  if (upperCaseChar) {
+    useChar = useChar.concat(upperCase);
+  }
   
-
   //Choose numeric
-  numeric =
+  numericChar =
   window.confirm("Would you like numeric characters in your password?");
+  if (numericChar) {
+    useChar = useChar.concat(numeric);
+  }
 
   //choose special characters
-  specialChar =
+  specialCharr =
   window.confirm("Would you like special characters in your password?");
+  if (specialCharr) {
+    useChar = useChar.concat(specialChar);
+  }
+
 };
 
 var checkCriteria = function() {
   if (
-    lowerCase === true ||
-    upperCase === true ||
-    numeric === true ||
-    specialChar === true 
+    lowerCaseChar === true ||
+    upperCaseChar === true ||
+    numericChar === true ||
+    specialCharr === true 
   ){
-    randomCriteria();
+    randomPassword();
   }
   else {
     window.alert("At least one character type should be selected. Please try again.");
@@ -66,10 +79,11 @@ var checkCriteria = function() {
   }
 };
 
-
-
+var randomPassword = function() {
+ return passwordCriteria[Math.floor(Math.random() * passwordCriteria.length)];
   
- 
+};
+  
 
 
 /* Generate Password*/
